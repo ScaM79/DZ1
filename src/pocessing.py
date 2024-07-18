7e6483dd48b622b94c3bc5ec84e60efb6cd19539
Danilov Stanislav <scam79@mail.ru>
2024-07-18 18:41:15 +0700

    add new function sort_by_date

--- src/pocessing.py
+++ src/pocessing.py
@@ -5,3 +5,8 @@ def filter_by_state(operations: list[dict], state: str='EXECUTED') ->list[dict]:
         if operation['state'] == state:
             filter_operations.append(operation)
     return filter_operations
+
+
+def sort_by_date(operations: list[dict], is_reverse: bool=True) -> list[dict]:
+    """Функция сортировки по дате"""
+    return sorted(operations, key = lambda operation: operation['date'], reverse = is_reverse)
