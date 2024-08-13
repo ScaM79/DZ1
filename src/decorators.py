07835c5b698467b5c5581bbc74a053d4778a0cc4
Stanislav Danilov <scam79@mail.ru>
2024-08-13 21:46:36 +0700

    отредактировал, что бы при отсутствии имени файла, логи выводились в консоль

--- src/decorators.py
+++ src/decorators.py
@@ -1,28 +1,28 @@
-import os
 import datetime
 from functools import wraps
 from typing import Any, Callable
 
 
-def log(filename: Any) -> Callable:
-    """
-    Декоратор логирования вызова функции и записи результата в файл или в консоль
-    """
-
+def log(filename: Any = None) -> Callable:
+    """Декоратор логирования вызова функции и записи результата в файл или вывода в консоль"""
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
                 log_message = f"{my_string}: {func.__name__} called with args: {args}, kwargs:{kwargs}. Result: {result}"
-                with open(filename, "a", encoding='utf-8') as f:
-                    f.write(log_message + "\n")
-                print(log_message)
+                if filename:
+                    with open(filename, "w", encoding='utf-8') as f:
+                        f.write(log_message + "\n")
+                else:
+                    print(log_message)
             except Exception as e:
                 error_message = f"{my_string}: {func.__name__} error: {e}. Inputs:{args}, {kwargs}"
-                with open(filename, "a", encoding='utf-8') as f:
-                    f.write(error_message + "\n")
-                print(error_message)
+                if filename:
+                    with open(filename, "w", encoding='utf-8') as f:
+                        f.write(error_message + "\n")
+                else:
+                    print(error_message)
 
         return wrapper
 
