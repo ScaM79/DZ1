d20d4421a1b679b8fb0c1db8b08a1a249ff409a5
Stanislav Danilov <scam79@mail.ru>
2024-08-13 21:47:35 +0700

    добавил тестирование без имени файла

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,12 +1,8 @@
-import os
-import datetime
-from functools import wraps
-from typing import Any, Callable
 from src.decorators import log
 
 
 def test_log(capsys):
-    @log(filename="./logs/test_log.txt")
+    @log()
     def my_function(x, y):
         return x + y
 
@@ -17,8 +13,15 @@ def test_log(capsys):
         "my_function called with args: (1, 2), kwargs:{}. Result: 3\n" in captured.out
     )
     # Проверка ошибки
-    try:
-        my_function(0, 2)
-    except TypeError:
-        captured = capsys.readouterr()
-        assert "my function error: " in captured.out
+
+
+def test_log():
+    @log(filename='logs/test_log.txt')
+    def my_function(x, y):
+        return x + y
+
+    # Проверка корректного выполнения функции
+    my_function(1, 2)
+    with open('logs/test_log.txt', "r", encoding='utf-8') as f:
+        result = f.read()
+    assert 'my_function called with args: (1, 2), kwargs:{}. Result: 3' in result
